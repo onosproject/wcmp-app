ab1619d20de2ab02cfc44681c00585d8235962c5
adibrastegarnia <adib.rastegarnia@intel.com>
2022-07-18 11:23:42 -0700

    Add pipeline configuration controller

---
+++ pkg/controller/configuration/watcher.go
@@ -0,0 +1,69 @@
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package configuration
+
+import (
+	"context"
+	topoapi "github.com/onosproject/onos-api/go/onos/topo"
+	"github.com/onosproject/onos-lib-go/pkg/controller"
+	"github.com/onosproject/onos-lib-go/pkg/logging"
+	"github.com/onosproject/wcmp-app/pkg/store/topo"
+	"sync"
+)
+
+var log = logging.GetLogger()
+
+const queueSize = 100
+
+// TopoWatcher is a topology watcher
+type TopoWatcher struct {
+	topo   topo.Store
+	cancel context.CancelFunc
+	mu     sync.Mutex
+}
+
+// Start starts the topo store watcher
+func (w *TopoWatcher) Start(ch chan<- controller.ID) error {
+	w.mu.Lock()
+	defer w.mu.Unlock()
+	if w.cancel != nil {
+		return nil
+	}
+
+	eventCh := make(chan topoapi.Event, queueSize)
+	ctx, cancel := context.WithCancel(context.Background())
+
+	err := w.topo.Watch(ctx, eventCh, nil)
+	if err != nil {
+		cancel()
+		return err
+	}
+	w.cancel = cancel
+	go func() {
+		for event := range eventCh {
+			log.Infow("Received topo event", "topo object ID", event.Object.ID)
+			if _, ok := event.Object.Obj.(*topoapi.Object_Entity); ok {
+				log.Infow("Event entity", "entity", event.Object)
+				// If the entity object has configurable aspect then the controller
+				// can make a connection to it
+				err = event.Object.GetAspect(&topoapi.P4RTServerInfo{})
+				if err == nil {
+					ch <- controller.NewID(event.Object.ID)
+				}
+			}
+		}
+	}()
+	return nil
+}
+
+// Stop stops the topology watcher
+func (w *TopoWatcher) Stop() {
+	w.mu.Lock()
+	if w.cancel != nil {
+		w.cancel()
+		w.cancel = nil
+	}
+	w.mu.Unlock()
+}
