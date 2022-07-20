10f633ab1465b935bbefd9d266d17f2a9dfcaef3
Deepak <eesha.deepak@intel.com>
2022-07-20 10:59:51 -0700

    adding integration tests

---
+++ test/p4rt/suite.go
@@ -0,0 +1,42 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package config
+
+import (
+	"github.com/onosproject/helmit/pkg/input"
+	"github.com/onosproject/helmit/pkg/test"
+	"github.com/onosproject/wcmp-app/test/utils/charts"
+)
+
+type testSuite struct {
+	test.Suite
+}
+
+// TestSuite is the onos-config GNMI test suite
+type TestSuite struct {
+	testSuite
+}
+
+func getInt(value interface{}) int64 {
+	if i, ok := value.(int); ok {
+		return int64(i)
+	} else if i, ok := value.(float64); ok {
+		return int64(i)
+	} else if i, ok := value.(int64); ok {
+		return i
+	}
+	return 0
+}
+
+// SetupTestSuite sets up the onos-config GNMI test suite
+func (s *TestSuite) SetupTestSuite(c *input.Context) error {
+	registry := c.GetArg("registry").String("")
+	umbrella := charts.CreateUmbrellaRelease()
+	r := umbrella.
+		Set("global.image.registry", registry).
+		Set("import.onos-cli.enabled", false). // not needed - can be enabled by adding '--set onos-umbrella.import.onos-cli.enabled=true' to helmit args for investigation
+		Install(true)
+	return r
+}
---
+++ test/utils/charts/umbrella.go
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: 2020-present Open Networking Foundation <info@opennetworking.org>
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package charts
+
+import (
+	"github.com/onosproject/helmit/pkg/helm"
+	"github.com/onosproject/onos-test/pkg/onostest"
+)
+
+// CreateUmbrellaRelease creates a helm release for an onos-umbrella instance
+func CreateUmbrellaRelease() *helm.HelmRelease {
+	return helm.Chart("onos-umbrella", onostest.OnosChartRepo).
+		Release("onos-umbrella").
+		Set("import.onos-gui.enabled", false).
+		Set("import.onos-config.enabled", false).
+		Set("onos-topo.image.tag", "latest")
+}
